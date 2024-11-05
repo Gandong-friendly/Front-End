3af758036bb462cf4a394bd45230f31dca06af02
KoreanCow <vue-cli@kakao.com>
2024-11-05 14:30:00 +0900

    feat: next-auth callbacks example

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -7,5 +7,21 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.KAKAO_CLIENT_ID as string,
       clientSecret: process.env.KAKAO_CLIENT_SECRET as string
     })
-  ]
+  ],
+  // testing
+  session: {
+    strategy: 'jwt',
+  },
+  // callbacks: {
+  //   async signIn ({user}) {
+  //     const response = await fetch(``, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({name : user.name}),
+  //     });
+
+  //   }
+  // }
 }
